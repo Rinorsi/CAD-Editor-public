04e2e6999484be1de744ca52473ff678e1efa6ef
Rinorsi <3144995399@qq.com>
2025-11-16 19:30:47 +0800

    骑乘编辑

--- common/src/main/java/com/github/rinorsi/cadeditor/client/screen/model/EntityEditorModel.java
+++ common/src/main/java/com/github/rinorsi/cadeditor/client/screen/model/EntityEditorModel.java
@@ -26,7 +26,6 @@ import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.OwnableEntity;
-import net.minecraft.world.entity.Saddleable;
 
 public class EntityEditorModel extends StandardEditorModel {
     public EntityEditorModel(EntityEditorContext context) {
@@ -107,17 +106,17 @@ public class EntityEditorModel extends StandardEditorModel {
     }
 
     private boolean hasMountData(Entity entity) {
-        if (entity instanceof Saddleable) {
+        if (entity instanceof LivingEntity) {
             return true;
         }
         CompoundTag tag = getContext().getTag();
         if (tag == null) {
             return false;
         }
-        return tag.contains("Saddle")
+        return tag.contains("Passengers", Tag.TAG_LIST)
+                || tag.contains("Saddle")
                 || tag.contains("Saddled")
                 || tag.contains("SaddleItem", Tag.TAG_COMPOUND)
-                || tag.contains("Passengers", Tag.TAG_LIST)
                 || tag.contains("Leash")
                 || tag.contains("LeashHolder")
                 || tag.contains("ChestedHorse");
