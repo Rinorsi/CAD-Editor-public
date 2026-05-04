4ae4c22c1fc9c760892d9270f845b5de57953021
Mgazul <mgazul@163.com>
2026-05-04 22:25:52 +0800

    修复无视权限刷物品

--- common/src/main/java/com/github/rinorsi/cadeditor/common/logic/ServerVaultActionLogic.java
+++ common/src/main/java/com/github/rinorsi/cadeditor/common/logic/ServerVaultActionLogic.java
@@ -1,11 +1,16 @@
 package com.github.rinorsi.cadeditor.common.logic;
 
 import com.github.rinorsi.cadeditor.common.CommonUtil;
+import com.github.rinorsi.cadeditor.common.ModTexts;
 import com.github.rinorsi.cadeditor.common.network.GiveVaultItemPacket;
 import net.minecraft.server.level.ServerPlayer;
 
 public class ServerVaultActionLogic {
     public static void onGiveVaultItem(ServerPlayer player, GiveVaultItemPacket response) {
+        if (!PermissionLogic.hasPermission(player)) {
+            CommonUtil.showPermissionError(player, ModTexts.VAULT);
+            return;
+        }
         player.getInventory().setItem(response.slot(), response.itemStack());
         CommonUtil.showVaultItemGiveSuccess(player);
     }
